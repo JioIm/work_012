d64abe3d033d054862100e371cbfc3df1c433a10
Jio Im <148739315+JioIm@users.noreply.github.com>
2024-01-17 00:36:26 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -2,4 +2,18 @@ $(function () {
     $('.MainVisual .main_visual_slide').slick({
         arrows: false,
     });
+
+    $('.main_tab li').on('click', function (e) {
+        e.preventDefault();
+        const idx = $(this).index();
+        $('.tab_content .content')
+            .eq(idx)
+            .addClass('on')
+            .siblings()
+            .removeClass('on');
+        $(this)
+            .addClass('on')
+            .siblings()
+            .removeClass('on');
+    });
 })
