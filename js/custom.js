d9ff978a99703984faebe6b79b3b761a1514b3e0
Jio Im <148739315+JioIm@users.noreply.github.com>
2024-01-18 01:10:07 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,13 @@
 $(function () {
+    $(window).on('scroll', function (){
+        const scrollHeight = $(window).scrollTop();
+        if(scrollHeight > 0) {
+        $('.header').addClass('on')
+        }else {
+        $('.header').removeClass('on')
+        }
+   });
+
     $('.MainVisual .main_visual_slide').slick({
         arrows: false,
     });
@@ -17,8 +26,28 @@ $(function () {
             .removeClass('on');
     });
 
+    $('.MainContent .left_slide').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.right_slide'
+      });
+      $('.MainContent .right_slide').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.left_slide',
+        dots: false,
+        centerMode: true,
+        focusOnSelect: true
+      });
+
     $('.MainInfo .itm').on('click', function (e) {
         e.preventDefault();
         $(this).addClass('on').siblings().removeClass('on');
     });
+
+
+
+    
 })
